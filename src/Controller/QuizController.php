701ca564c211b7315ca5f11a4fe8e090dfe2a343
AscheriitAdventure <94352743+AscheriitAdventure@users.noreply.github.com>
2023-10-11 15:44:30 +0200

    feat: controllerQuiz

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Anonymous;
+use App\Form\RegistrationFormType;
+use App\Security\AppAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +19,22 @@ class QuizController extends AbstractController
     {
         return $this->render('quiz/index.html.twig', [
             'controller_name' => 'QuizController',
+        ]);
+    }
+    #[Route('/quiz', name: 'app_quiz-enter')]
+    public function startquiz(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new Anonymous();
+        $form = $this->createForm(AnonymousFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('quiz/index.html.twig', [
+            'anonymousForm' => $form->createView(),
         ]);
     }
 }
